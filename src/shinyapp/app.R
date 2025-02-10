b1335954c0ffa080a0535f1ed90baf3601a5468d
Tony Hirst <tony.hirst@gmail.com>
2025-02-10 14:37:57 +0000

    Add stages data

--- src/shinyapp/app.R
+++ src/shinyapp/app.R
@@ -6,6 +6,11 @@ library(purrr)
 library(leaflet)
 source("geotools.R")
 
+# TO DO
+# THis looks interesting: https://shinystoreplus.obi.obianom.com/
+# - eg save map data etc to browser storage
+# - eg pick up vals from URL so we can share a view for a year/event/stage
+
 # Function to extract a specific meta value
 extract_meta_value <- function(meta_list, key) {
   value <- meta_list$v[meta_list$n == key]
@@ -15,7 +20,7 @@ extract_meta_value <- function(meta_list, key) {
   return(value)
 }
 
-SEASON_URL = "https://webappsdata.wrc.com/srv/wrc/json/api/wrcsrv/byType?t=%22Season%22&maxdepth=2"
+SEASON_URL <- "https://webappsdata.wrc.com/srv/wrc/json/api/wrcsrv/byType?t=%22Season%22&maxdepth=2"
 
 # Data loading and processing function
 load_event_data <- function() {
@@ -30,23 +35,38 @@ load_event_data <- function() {
     mutate(
       year = name,
       event_name = `_dchildren_name`,
+      event_id = `_dchildren__id`,
       event_start = map_chr(`_dchildren__meta`, extract_meta_value, key = "date-start"),
       event_finish = map_chr(`_dchildren__meta`, extract_meta_value, key = "date-finish"),
       event_surface = map_chr(`_dchildren__meta`, extract_meta_value, key = "info-surface"),
       kmlmap = map_chr(`_dchildren__meta`, extract_meta_value, key = "kmlfile"),
       time_zone = map_chr(`_dchildren__meta`, extract_meta_value, key = "time-zone"),
-      "category" = map_chr(`_dchildren__meta`, extract_meta_value, key = "category"),
+      category = map_chr(`_dchildren__meta`, extract_meta_value, key = "category"),
       rally_id = map_chr(`_dchildren__meta`, extract_meta_value, key = "Event")
     ) %>%
-    select(year, event_name, event_start, event_finish, event_surface, kmlmap, time_zone, category, rally_id)
-
-# TO DO - if we look in _children we get IDs we can look up using 
-# https://webappsdata.wrc.com/srv/wrc/json/api/wrcsrv/byId?id=%22{id}%22
-# Each record has the stage info
-# If we look up the season to depth 3, we can recurse into a _dchildren
-# in each event record to get the stages info
+    select(year, event_id, event_name, event_start, event_finish, event_surface, kmlmap, time_zone, category, rally_id)
 
   return(event_data)
+}
+
+# Data loading and processing function
+load_stages_data <- function(event_id) {
+  event_url <- paste0("https://webappsdata.wrc.com/srv/wrc/json/api/wrcsrv/byId?id=%22", event_id, "%22&maxdepth=2")
+  # Read JSON data from URL
+  json_data <- fromJSON(event_url)
+
+  # TO DO - using the event_id, we can look up eg
+  # https://webappsdata.wrc.com/srv/wrc/json/api/wrcsrv/byId?id=%226ad7a7fa-da2c-43bb-93c5-da788d0ab7a5%22&maxdepth=2
+  # and use the same pattern as above to get stage info from _dchldren__meta:
+  # name, location, distance, date, firstCar, kmltrack
+  # print(json_data$`_dchildren`)
+  df <- as_tibble(json_data$`_dchildren`)
+  df2 <- df %>%
+    unnest(`_meta`) %>%
+    select(-`_dchildren`) %>%
+    pivot_wider(names_from = n, values_from = v, names_repair = "unique")
+  # print(head(df2))
+  return(df2 %>% select(`start-time-control`, location, distance, date, firstCar, kmlfile, kmltrack))
 }
 
 # UI definition
@@ -70,6 +90,11 @@ ui <- fluidPage(
       selectInput("event_select",
         "Select Event:",
         choices = NULL
+      ),
+      # Event selection - dynamically updated
+      selectInput("stage_select",
+        "Select Stage:",
+        choices = NULL
       )
     ),
     mainPanel(
@@ -80,6 +105,7 @@ ui <- fluidPage(
         htmlOutput("event_dates"),
         textOutput("event_surface"),
         textOutput("rally_id"),
+        tableOutput("stage_table"),
         leafletOutput("event_map")
       )
     )
@@ -148,22 +174,39 @@ server <- function(input, output, session) {
     )
   })
 
+  # Update stage dropdown based on selected year, category and event
+  observe({
+    req(input$year_select, input$category_select, input$event_select)
+
+    stages <- load_stages_data(filtered_event_info()$event_id) %>%
+      pull(`start-time-control`)
+
+    updateSelectInput(session, "stage_select",
+      choices = stages
+    )
+  })
+
   # Display selected event details
   output$event_name <- renderText({
     req(input$event_select)
     paste("Event:", input$event_select)
   })
 
-filtered_event_info <- reactive({
-  req(input$event_select, input$year_select)
+  filtered_event_info <- reactive({
+    req(input$event_select, input$year_select)
 
-  event_data() %>%
-    filter(
-      event_name == input$event_select,
-      year == input$year_select
-    ) %>%
-    head(1)
-})
+    event_data() %>%
+      filter(
+        event_name == input$event_select,
+        year == input$year_select
+      ) %>%
+      head(1)
+  })
+
+  stages_info <- reactive({
+    req(input$event_select, input$year_select)
+    load_stages_data(filtered_event_info()$event_id)
+  })
 
   output$event_dates <- renderUI({
     HTML(paste(
@@ -177,9 +220,13 @@ filtered_event_info <- reactive({
     paste("Surface:", filtered_event_info()$event_surface)
   })
 
-output$rally_id <- renderText({
-  paste("rallyId:", filtered_event_info()$rally_id)
-})
+  output$rally_id <- renderText({
+    paste("rallyId:", filtered_event_info()$rally_id)
+  })
+
+  output$stage_table <- renderTable({
+    stages_info()
+  })
 
   output$event_map <- renderLeaflet({
     # Get the urlstub from the event_info
@@ -198,7 +245,6 @@ output$rally_id <- renderText({
       ) %>%
       addPolylines(color = "red", weight = 4)
   })
-
 }
 
 # Run the app
